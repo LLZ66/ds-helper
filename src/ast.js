73490e8f2e40e5f4ab97072be1e9c3c0386117ae
刘涟洲 <1228429427@qq.com>
2023-06-03 15:39:43 +0800

    FEAT:增加npx命令

--- src/ast.js
+++ src/ast.js
@@ -2,7 +2,7 @@
  * @Author: 刘涟洲 1228429427@qq.com
  * @Date: 2022-09-08 10:49:35
  * @LastEditors: 刘涟洲 1228429427@qq.com
- * @LastEditTime: 2022-10-13 10:09:08
+ * @LastEditTime: 2023-05-30 19:07:16
  * @FilePath: \ds-helper\src\utils\ast.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ function generateDSAst(codePart) {
     const tsFilePath = path.join(__dirname, 'tscode.ts');
     fs.writeFileSync(tsFilePath, code);
     try {
-      child_process.execSync(`tsc ${tsFilePath}`);
+      child_process.execSync(`npx tsc ${tsFilePath}`);
     }catch(err) {};
     const fileContent = fs.readFileSync(path.join(__dirname, 'tscode.js'));
     const dsAst = acorn.parse(fileContent, {
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,8 @@
 <Form>
-  <TextField name="modelName" />
-	<TextField name="count" />
+  <TextField name="statusMeaning" />
+	<TextField name="keywords" />
+	<TextField name="companyName" />
+	<TextField name="organizationName" />
+	<TextField name="factoryName" />
+	<TextField name="createTime" />
 </Form>
